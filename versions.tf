4dc92dd83b634a49fbeb883f75c229ebcb57bcc1
Jan Jungnickel <jan@jungnickel.com>
2020-09-07 10:52:41 +0200

    Terraform 0.13

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.5.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 2.1.2"
+    }
+  }
+  required_version = ">= 0.13"
+}
