8c96a965a40255d76a2abaaee2174082ec7254be
Jan Jungnickel <jan@jungnickel.com>
2021-06-10 16:55:25 +0200

    fix: remove interpolation-only expressions, take two

--- output.tf
+++ output.tf
@@ -29,6 +29,6 @@ output "policy_id" {
 }
 
 output "switchrole_urls" {
-  description = "List of URL to the IAM console to switch to the roles"
-  value       = ["${formatlist(var.switchrole_url_template, null_resource.role.*.triggers.account_id, null_resource.role.*.triggers.role_name, null_resource.role.*.triggers.alias)}"]
+  description = "List of URLs to the AWS console to switch to the roles"
+  value       = formatlist(var.switchrole_url_template, null_resource.role.*.triggers.account_id, null_resource.role.*.triggers.role_name, null_resource.role.*.triggers.alias)
 }
